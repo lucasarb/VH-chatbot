475d84d3805891aa91bb411ba9206a9bc6c81d0a
Lucas Aragão Bessa <lab.aragao@gmail.com>
2016-05-22 00:21:14 -0300

    Multiples Threads added, Guessing game as a test

--- bot_test.py
+++ bot_test.py
@@ -1,19 +1,51 @@
 import sys
-import time
-import pprint
+import random
+import traceback
 import telepot
+from telepot.delegate import per_chat_id, create_open
 
-BOT_TOKEN = '228795069:AAEDYFUP96p8SUpDNlcqGSmksKrixKkGmD8'
+class Player(telepot.helper.ChatHandler):
+	def __init__(self, seed_tuple, timeout):
+		super(Player,self).__init__(seed_tuple,timeout)
+		self._answer = random.randint(0,99)
 
-def handle(msg):
-	
-	return
+	def _hint(self, answer, guess):
+		if answer > guess:
+			return 'larger'
+		else:
+			return 'smaller'
 
-bot = telepot.Bot(BOT_TOKEN)
+	def open(self, initial_msg, seed):
+		self.sender.sendMessage('Guess my number')
+		return True
 
-bot.message_loop(handle)
-print 'Listening...'
+	def on_chat_message(self, msg):
+		content_type, chat_type, chat_id = telepot.glance(msg)
 
-while True:
-	time.sleep(30)
+		if content_type != 'text':
+			self.sender.sendMessage('Give me a number, please.')
+			return
+		
+		try:
+			guess = int(msg['text'])
+		except ValueError:
+			self.sender.sendMessage('Give me number, please.')
+			return
 
+		if guess != self._answer:
+			#give a hint
+			hint = self._hint(self._answer,guess)
+			self.sender.sendMessage(hint)
+		else:
+			self.sender.sendMessage('Correct!!')
+			self.close()
+	def on_close(self, exception):
+		if isinstance(exception, telepot.exception.WaitTooLong):
+			self.sender.sendMessage('Game expired. The answer is %d' % self._answer)
+
+
+TOKEN = '228795069:AAEDYFUP96p8SUpDNlcqGSmksKrixKkGmD8'
+
+bot = telepot.DelegatorBot(TOKEN, [(per_chat_id(),create_open(Player, timeout = 30))])
+
+bot.message_loop(run_forever = True)
